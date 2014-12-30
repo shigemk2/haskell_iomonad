3b4ff3cdaddd9d0744104f33c9c6aad83e7292cf
shigemk2 <i.am.shige@gmail.com>
2014-12-30 17:31:26 +0900

    コメント追加

--- ghcbase.hs
+++ ghcbase.hs
@@ -1,13 +1,21 @@
 import GHC.Base
 
 main = do
+    -- 値をIOモナドに格納する
     let m  = return 1
+    -- IOモナドから値を関数に渡す
     print =<< m
 
+    -- IOモナドから関数を取り出す
     let f  = unIO m
+    -- 関数をIOモナドに格納する
     let m1 = IO f
+    -- IOモナドから値を関数に渡す
     print =<< m1
 
+    -- IOモナドから値を取り出す
     v <- m
+    -- 値をIOモナドに格納する
     let m2 = return v
+    -- IOモナドから値を関数に渡す
     print =<< m2
