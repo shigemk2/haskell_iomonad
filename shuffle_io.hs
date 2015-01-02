a57d7dd12400ea6bdf686b1808d60ea00a5f52c1
shigemk2 <i.am.shige@gmail.com>
2015-01-02 17:00:58 +0900

    とりあえずunIOを使ったシャッフル

---
+++ shuffle_io.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE UnboxedTuples #-}
+import GHC.Base
+import System.Random
+
+shuffle [] = return []
+shuffle xs = do
+    n <- getStdRandom $ randomR (0, length xs - 1) :: IO Int
+    xs' <- shuffle $ take n xs ++ drop (n + 1) xs
+    return $ (xs !! n) : xs'
+
+main = IO $ \s ->
+    let (# s1, r #) = unIO (print =<< shuffle [1..9]) s
+    in  (# s1, r #)
