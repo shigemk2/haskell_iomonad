f727d5ad8d06b3f502f23378518e504a3b7fc08e
shigemk2 <i.am.shige@gmail.com>
2015-01-02 16:35:21 +0900

    シャッフルするやつ

---
+++ shuffle.hs
@@ -0,0 +1,11 @@
+import System.Random
+
+shuffle [] = return []
+shuffle xs = do
+    n <- getStdRandom $ randomR (0, length xs - 1) :: IO Int
+    xs' <- shuffle $ take n xs ++ drop (n + 1) xs
+    return $ (xs !! n) : xs'
+
+main = do
+    xs <- shuffle [1..9]
+    print xs
